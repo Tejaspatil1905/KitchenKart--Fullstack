99b27feae0ef307e81eb59c00b90f8ed28d7f9d4
Tejaspatil <tejasppatil1905@gmail.com>
2025-10-09 22:58:36 +0530

    Rename fooditem.jsx to FoodItem.jsx to match import

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../Context/StoreContext";
-import FoodItem from "../FoodItem/fooditem.jsx";
+import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { foodList } = useContext(StoreContext);
